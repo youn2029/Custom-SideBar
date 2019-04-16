8651826450157f102775bf21cc026f93fda3595e
youn2029 <cocoa8090@gmail.com>
2019-04-16 14:55:15 +0900

    사이드 바를 사용하기 위한 스토리보드 설정

---
+++ Custom-SideBar/FrontViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FrontViewController.swift
+//  Custom-SideBar
+//
+//  Created by 윤성호 on 16/04/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+
+    // 사이드 바 버튼
+    @IBOutlet var sideBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
